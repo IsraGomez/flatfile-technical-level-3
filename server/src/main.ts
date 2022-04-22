3e20e26ccd7b6b2ef65395e4860192b8b426cca3
IsraGomez <igomez@signpost.com>
2022-04-22 17:07:32 -0500

    fix(server/src/main)!: eliminated CORS protection
    BREAKING CHANGE: might not be the behavior wanted, but couldn't POST with the origin.
    
    preflightContinue is by default false, so the statement is redundant.

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,10 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: true,
     cors: (req, callback) => {
-      callback(null, {
-        origin: req.method !== 'POST',
-        preflightContinue: false,
-      })
+      callback(null, {})
     },
   })
   await app.listen(3001)
